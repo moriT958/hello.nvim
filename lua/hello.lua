8a0291d2dde92f2deb2220a34c978f7e8d297069
moriT958 <morit958@icloud.com>
2025-11-04 23:18:57 +0900

    feat: フローティングウィンドウを作成

--- lua/hello.lua
+++ lua/hello.lua
@@ -1,7 +1,23 @@
 local M = {}
 
 function M.hello()
-  print("Hello, World!!!")
+  local msg = "Hello, World 🤠"
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { msg })
+
+  local width = 20
+  local height = 1
+  local ops = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = (vim.o.columns - width) / 2,
+    row = (vim.o.lines - height) / 2 - 1,
+    style = "minimal",
+    border = "rounded",
+  }
+  vim.api.nvim_open_win(buf, true, ops)
 end
 
 return M
